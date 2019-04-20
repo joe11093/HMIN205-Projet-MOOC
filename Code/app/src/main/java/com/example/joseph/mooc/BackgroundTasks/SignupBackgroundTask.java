cc0ea04abc129c691fec6245a41d3b12097f3798
joe11093 <joe11093@users.noreply.github.com>
2019-04-21 00:57:11 +0200

    Added the AsyncTask implementation for the signup process

---
+++ Code/app/src/main/java/com/example/joseph/mooc/BackgroundTasks/SignupBackgroundTask.java
@@ -0,0 +1,17 @@
+package com.example.joseph.mooc.BackgroundTasks;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import com.example.joseph.mooc.R;
+
+public class SignupBackgroundTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    
+}
+
