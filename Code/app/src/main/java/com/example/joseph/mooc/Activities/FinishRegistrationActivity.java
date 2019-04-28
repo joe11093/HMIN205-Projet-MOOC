2f1d60dcee8c8cfc5ee4f2f4247cfca86f922f36
joe11093 <joe11093@users.noreply.github.com>
2019-04-28 14:31:36 +0200

    added the finish registration activity

---
+++ Code/app/src/main/java/com/example/joseph/mooc/Activities/FinishRegistrationActivity.java
@@ -0,0 +1,37 @@
+package com.example.joseph.mooc.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.joseph.mooc.Models.Parent;
+import com.example.joseph.mooc.R;
+
+import org.w3c.dom.Text;
+
+//this class finalizes the registration
+//insert the students and parents into the database
+
+public class FinishRegistrationActivity extends AppCompatActivity {
+
+    Intent intent;
+    Bundle bundle;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Finishregistration", "Finish registration Activity oncreate()");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_registration);
+
+        this.intent = getIntent();
+        this.bundle = this.intent.getExtras();
+
+        //test
+        this.tv = findViewById(R.id.textView4);
+        Parent parent = (Parent) bundle.get("parent");
+        tv.setText("Registering: " + parent.getFirstname() + " " + parent.getLastname());
+    }
+}
