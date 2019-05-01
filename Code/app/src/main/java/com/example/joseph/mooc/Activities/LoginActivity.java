4399b539746476542d6fac22a56f17e9a5382255
joe11093 <joe11093@users.noreply.github.com>
2019-05-01 16:49:58 +0200

    Log in Activity

--- Code/app/src/main/java/com/example/joseph/mooc/Activities/LoginActivity.java
+++ Code/app/src/main/java/com/example/joseph/mooc/Activities/LoginActivity.java
@@ -1,44 +1,96 @@
 package com.example.joseph.mooc.Activities;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.joseph.mooc.BackgroundTasks.BackgroundTask;
-import com.example.joseph.mooc.R;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.joseph.mooc.BackgroundTasks.BackgroundTask;
+import com.example.joseph.mooc.BackgroundTasks.LoginAsyncClass;
+import com.example.joseph.mooc.Helper.GlobalProperties;
+import com.example.joseph.mooc.Interfaces.Callback;
+import com.example.joseph.mooc.Models.*;
+import com.example.joseph.mooc.R;
+import com.example.joseph.mooc.Models.Login;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements Callback {
     //get username and password from user
     //check them in the DB
     //if correct, log in user by adding him to the shared preferences
 
-    EditText e_id;
-    EditText e_firstname;
-    String e_id_str, e_firstname_str;
+
+    EditText emailEditText, passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
 
-        e_id = (EditText) findViewById(R.id.id);
-        e_firstname = (EditText) findViewById(R.id.firstname);
+        //get the edit texts
+        this.emailEditText = findViewById(R.id.loginEmail);
+        this.passwordEditText = findViewById(R.id.loginPassword);
     }
 
     public void login(View v){
         Toast.makeText(this, "Log in function", Toast.LENGTH_SHORT).show();
+        String email = this.emailEditText.getText().toString();
+        String password = this.passwordEditText.getText().toString();
+        Login login = new Login(email, password);
 
-        e_id = (EditText) findViewById(R.id.id);
-        e_firstname = (EditText) findViewById(R.id.firstname);
-        e_id_str = e_id.getText().toString();
-        e_firstname_str  = e_firstname.getText().toString();
+        //execute the login async task
+        LoginAsyncClass loginAsyncClass = new LoginAsyncClass(this);
+        loginAsyncClass.execute(login);
+    }
 
-        String method = "login";
-        BackgroundTask backgroundTask = new BackgroundTask(this);
-        backgroundTask.execute(method, e_id_str, e_firstname_str);
-        finish();
+    @Override
+    public void processData(String data) {
+        if(data.equals(null)){
+            Toast.makeText(this, "NULL", Toast.LENGTH_LONG).show();
+        }
+        else{
+            if(data.equals("error")){
+                Toast.makeText(this, "NULL", Toast.LENGTH_LONG).show();
+                Log.d("loginerror", "error");
+            }
+            else{
+                //no error
+                Toast.makeText(this, data, Toast.LENGTH_LONG).show();
+                Log.d("loginresult", data);
 
+                //add info to shared preferences
+                SharedPreferences.Editor editor = getSharedPreferences(GlobalProperties.login_sharedpreferences, MODE_PRIVATE).edit();
+                data = data.replace("{", "");
+                data = data.replace("}", "");
+                String[] array = data.split(",");
+                String id = array[0].split(":")[1];
+                String firstname = array[2].split(":")[1];
+                String email = array[2].split(":")[1];
+                String type = array[2].split(":")[1];
+                Log.d("splitarray", id);
+                Log.d("splitarray", firstname);
+                Log.d("splitarray", email);
+                Log.d("splitarray", type);
+
+                editor.putString("id", id);
+                editor.putString("firstname", firstname);
+                editor.putString("email", email);
+                editor.putString("type", type);
+                editor.apply();
+
+                Intent intent = new Intent(this, UserProfileActivity.class);
+                startActivity(intent);
+
+            }
+        }
     }
 }
