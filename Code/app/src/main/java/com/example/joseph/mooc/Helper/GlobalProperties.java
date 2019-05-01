6a982c0944def7e40f6b914423c1d1c81d6e21fa
joe11093 <joe11093@users.noreply.github.com>
2019-05-01 16:50:29 +0200

    class for the general app settings

---
+++ Code/app/src/main/java/com/example/joseph/mooc/Helper/GlobalProperties.java
@@ -0,0 +1,13 @@
+package com.example.joseph.mooc.Helper;
+
+/**
+ * Created by josep on 4/28/2019.
+ */
+
+public final class GlobalProperties {
+
+    public final static String login_sharedpreferences = "login_info";
+
+    private GlobalProperties() {
+    }
+}
