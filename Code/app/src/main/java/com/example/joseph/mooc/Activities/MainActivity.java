7138c967c129d68cbb271a89966ae33fbf871a54
joe11093 <joe11093@users.noreply.github.com>
2019-05-01 16:48:48 +0200

    changed the link on the menu

--- Code/app/src/main/java/com/example/joseph/mooc/Activities/MainActivity.java
+++ Code/app/src/main/java/com/example/joseph/mooc/Activities/MainActivity.java
@@ -11,7 +11,7 @@ import com.example.joseph.mooc.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    //CHECK SHARED PREFERENCES
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void viewSampleVideo(View v){
 
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=Hxy8BZGQ5Jo")));
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=3LdRZJja7B8&list=PLolGVm_X3HO3UMdfLe_iTo41C2Hge_PS4")));
         Log.i("Video", "Video Playing....");
 
     }
