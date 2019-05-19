cc8d1f39dad872081bbac972cc18e6d0296b52b9
joe11093 <joe11093@users.noreply.github.com>
2019-05-20 01:43:27 +0200

    nothing

--- Code/app/src/main/java/com/example/joseph/mooc/Helper/GlobalProperties.java
+++ Code/app/src/main/java/com/example/joseph/mooc/Helper/GlobalProperties.java
@@ -1,5 +1,8 @@
 package com.example.joseph.mooc.Helper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by josep on 4/28/2019.
  */
@@ -8,6 +11,4 @@ public final class GlobalProperties {
 
     public final static String login_sharedpreferences = "login_info";
 
-    private GlobalProperties() {
-    }
 }
