22483f219303eed33c06e62dc81d483bb83507a3
joe11093 <joe11093@users.noreply.github.com>
2019-05-20 01:44:45 +0200

    fixed log text

--- Code/app/src/main/java/com/example/joseph/mooc/BackgroundTasks/LoginAsyncClass.java
+++ Code/app/src/main/java/com/example/joseph/mooc/BackgroundTasks/LoginAsyncClass.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import com.example.joseph.mooc.Helper.DBMooc;
+import com.example.joseph.mooc.Helper.GlobalProperties;
 import com.example.joseph.mooc.Interfaces.Callback;
 import com.example.joseph.mooc.Models.Login;
 import com.example.joseph.mooc.Models.User;
@@ -29,13 +30,14 @@ import java.nio.charset.Charset;
 public class LoginAsyncClass extends AsyncTask<Login, Void, String> {
 
     Callback cb;
-
     public LoginAsyncClass(Callback cb) {
         this.cb = cb;
+        Log.d("logintask", "Started the login asynctask");
     }
 
     @Override
     protected String doInBackground(Login... params) {
+        Log.d("logintask", "started doInBackground");
         String loginurl= DBMooc.baseUrl + "login.php";
 
         Object param = params[0];
@@ -45,6 +47,7 @@ public class LoginAsyncClass extends AsyncTask<Login, Void, String> {
         //result string
         String data = "";
         if(login != null) {
+            Log.d("logintask", "if(login != null)");
             try {
 
                 URL url = new URL(loginurl);
@@ -52,7 +55,7 @@ public class LoginAsyncClass extends AsyncTask<Login, Void, String> {
                 httpURLConnection.setRequestMethod("POST");
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
-
+                Log.d("logintask", "connecction is opened");
                 OutputStream os = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
 
@@ -60,28 +63,30 @@ public class LoginAsyncClass extends AsyncTask<Login, Void, String> {
                 json.put("email", login.getEmail());
                 json.put("password", login.getPassword());
                 data = json.toString();
-                //Log.d("json", data);
+                Log.d("logintask", "jsontostring: " + data);
 
                 bufferedWriter.write(data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
                 os.close();
-
+                Log.d("logintask", "after buffered writer close");
                 InputStream instream = httpURLConnection.getInputStream();
+                Log.d("logintask", "got the input stream");
                 byte[] bytes = null;
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
                 byte[] temp = new byte[instream.available()];
+                Log.d("logintask", "reading from input stream");
                 while (instream.read(temp, 0, temp.length) != -1) {
                     baos.write(temp);
                     temp = new byte[instream.available()];
                 }
-
+                Log.d("logintask", "finished reading the response");
                 bytes = baos.toByteArray();
                 String result = new String(bytes, Charset.forName("utf-8"));
-
+                Log.d("logintask", "result: " + result);
                 instream.close();
-                Log.d("resultLoginAsync", result);
+                Log.d("logintask", "finishing the DoInBackground function");
                 return result;
 
             } catch (MalformedURLException e) {
@@ -92,16 +97,19 @@ public class LoginAsyncClass extends AsyncTask<Login, Void, String> {
                 e.printStackTrace();
             }
         }
-        Log.d("returnNull", "returnNull");
+        Log.d("logintask", "returnNull");
         return null;
     }
 
     @Override
     protected void onPostExecute(String exists) {
-
+        Log.d("logintask", "started onPostExecute");
         super.onPostExecute(exists);
         //Log.d("onpostexecute", exists);
         //Toast.makeText(ctx, "Result: " + exists, Toast.LENGTH_LONG).show();
-        cb.processData(exists);
+        Log.d("logintask", "exists: = " + exists);
+        String code = "logintask";
+        Log.d("logintask", "code: = " + code);
+        cb.processData(code, exists);
     }
 }
